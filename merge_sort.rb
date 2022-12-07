8f5e2ee2c4e289371f029655925338f28d3ba866
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-12-07 16:19:54 -0700

    Create initial recursion logic

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,4 +1,14 @@
 def merge_sort(array)
-  
+  mid = (array.length - 1) / 2
+  if array.length < 2
+    return array
+  else
+    left = merge_sort(array[0..mid])
+    right = merge_sort(array[(mid + 1)..array[-1]])
 
+    left < right ? array.first = left : array.first = right
+  end
+  array
 end
+
+p merge_sort([3,1,0,9,5,10,3,2,7,0,8,7,3,1])
